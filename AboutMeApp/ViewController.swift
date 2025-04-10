2e4a6a3faf8f99379c627f6ecbd68262ff10e28a
Konstantin <diel404@gmail.com>
2025-04-10 20:57:49 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Konstantin on 10.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
